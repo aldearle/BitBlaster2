96a9a80610b01f1ef51dc1b999218dfef9b5249d
Alan Dearle <alan.dearle@st-andrews.ac.uk>
2022-03-30 10:15:06 +0100

    Initial commit

---
+++ src/uk/al_richard/BitBlaster2/Paritition.java
@@ -0,0 +1,19 @@
+package uk.al_richard.BitBlaster2;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @param <T> the type of values being modelled
+ *
+ *  This class represents the class of binary partitions of a metric space
+ */
+public abstract class Paritition<T> {
+
+    public Paritition( List<T> pivots, Set<T>  reference_objects ) {
+    }
+
+    abstract public double f();
+
+
+}
